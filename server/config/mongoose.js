b3d0f36123f7abe4808b5bcff4f2ac027e250c46
ohis <ohifemeigbarumah1@gmail.com>
2017-05-29 13:22:31 -0500

    modified

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,9 +1,16 @@
 var mongoose = require('mongoose');
 
 var fs  = require('fs');
-
-mongoose.connect('mongodb://localhost/Wall');
 mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Wall').then(
+	function(){
+		console.log("Connected to db");
+	},
+	function(){
+		console.log('FAILED TO CONNECT TO DB');
+	}
+);
+
 var models_path = __dirname + '/../models';
 
 fs.readdirSync(models_path).forEach(function(file){
